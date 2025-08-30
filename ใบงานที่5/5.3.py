5aa6329306c0c1fc9305fcbefcdecaa8cbd50ee8
yoyumyum <gusjailnwza004@gmail.com>
2025-08-30 19:17:41 +0700

    Add files via upload

---
+++ ใบงานที่5/5.3.py
@@ -0,0 +1,25 @@
+print("1.", end=" ")
+i= 1
+while (i < 22):
+    print(i, end=",")
+    i+=2
+print("\n2.", end=" ")
+i = 2
+while (i < 18):
+    print(i, end=",")
+    i+=3
+print("\n3.", end=" ")
+i = 100
+while (i > -101):
+    print(i, end=",")
+    i-=10
+print("\n4.", end=" ")
+i = -30
+while (i < 31):
+    print(i, end=",")
+    i+=10
+print("\n5.", end=" ")
+i = 15
+while (i < 56):
+    print(i, end=",")
+    i+=8
